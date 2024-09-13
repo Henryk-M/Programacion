4a7f3fd9b06d997407d41c9cf3a9ee69e6a3032c
Henryk-M <henryk.martynski@campus.xaloc.org>
2024-09-13 12:44:44 +0200

    Add files via upload

---
+++ Programacion/Python/Ejemplos/ejemplo python 2.py
@@ -0,0 +1,7 @@
+var_numero=3
+var:num=3.5
+var_texto="hola"
+var_total=var_numero+var_num
+#si añado la letra f antes del texto puedo visualizar el valor de las variables
+#siempre que las ponga entre {{{{{{{
+print(f"el total de {var_numero} y {var_num} es:", var_total)
---
+++ ejemplo python.py
@@ -0,0 +1,10 @@
+#trabajaremos con 3 tipos de variables; almoadilla para poner comentarios
+#en python el tipo de variable lo define el valor que ponga, no puedo poner espacios en blanco tengo que usar _ y aparte no puedo empezar una variable con número y para el texto se usan comillas ""
+var_numero=7
+var_num=3.5
+var_text="Racedriver_Grid"
+print(var_text)
+#formato 1 de print: poner textos entre comillas separados con comas
+print("el total es:",var_numero,"euros y el cambio es",var_num)
+#para ejecutar comandos ejecutamos con "Run" y los resultados apareceran en consola
+
