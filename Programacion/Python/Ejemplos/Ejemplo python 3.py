2ab3ad4b45dc36fa278607eb1500f9e1b6a16d90
Henryk-M <henryk.martynski@campus.xaloc.org>
2024-09-17 10:24:54 +0200

    Add files via upload

---
+++ Programacion/Python/Ejemplos/Ejemplo python 3.py
@@ -0,0 +1,45 @@
+var=3
+var2="hola"
+
+
+var3=input("Introduce el primer número: ")
+
+print("El valor introducido es un: " ,var3)
+
+var3=int(input("Introduce el primer número: ")
+
+var4=int(input("Introduce el segundo número: ")
+
+total=(var3+var4)
+
+print ("La suma es: " ,total
+
+
+
+var3=str(var3
+
+
+float #para trabajar con numeros decimales
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
