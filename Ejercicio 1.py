25c1466d13afc7f47d38db155124d4f8d285d8f6
Henryk-M <henryk.martynski@campus.xaloc.org>
2024-09-18 13:38:35 +0200

    Add files via upload

---
+++ Ejercicio 1.py
@@ -0,0 +1,3 @@
+#1. Programa que muestre por pantalla la frase “hello world”
+var1=("hello world")
+print(var1)
---
+++ Ejercico 2.py
@@ -0,0 +1,9 @@
+#2. Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla
+#en el siguiente orden: número entero, texto y número decimal.
+
+var1=(int(input("Introduce un número entero")))
+print("El valor introducido es un:" ,var1)
+var2=(input("Introduce un texto"))
+print("El texto introducido es :" ,var2)
+var3=(float(input("Introduce un número decimal")))
+print("El número decimal introducido es :" ,var3)
