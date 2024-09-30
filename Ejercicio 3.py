dfecbbfe5227e8435b0074d31a628a8b8a68b31d
Henryk-M <henryk.martynski@campus.xaloc.org>
2024-09-30 22:28:47 +0200

    Delete Ejercicio 3.py

--- Ejercicio 3.py
+++
@@ -1,4 +0,0 @@
-#3. Programa que pida dos números enteros y realice la suma correspondiente
-var1=(int(input("Introduce el primer número entero")))
-var2=(int(input("Introduce el segundo número entero")))
-print("El resultado de la suma es:" ,var1+var2)
